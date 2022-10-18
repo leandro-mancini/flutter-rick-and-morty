fd3a845fdf1d79345e3380ffe7b69ef28dfdc6b6
Leandro Mancini <mancinilele@gmail.com>
2022-10-18 19:03:41 -0300

    feat(app): add route characters

--- apps/flutter/suflex/lib/src/app_module.dart
+++ apps/flutter/suflex/lib/src/app_module.dart
@@ -1,6 +1,9 @@
+import 'package:flutter_characters/flutter_characters.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
 class AppModule extends Module {
   @override
-  List<ModularRoute> get routes => [];
+  List<ModularRoute> get routes => [
+    ModuleRoute('/', module: CharactersModule())
+  ];
 }
--- libs/flutter/characters/lib/src/characters_module.dart
+++ libs/flutter/characters/lib/src/characters_module.dart
@@ -1,7 +1,7 @@
 import 'package:flutter_characters/src/characters_page.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
-class Characters extends Module {
+class CharactersModule extends Module {
   @override
   List<ModularRoute> get routes => [
     ChildRoute('/', child: (context, args) => const CharactersPage())

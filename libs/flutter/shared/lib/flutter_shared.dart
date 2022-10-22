80768b07409072d5bb8deeb59deacb4b7c5d5f13
Leandro Mancini <mancinilele@gmail.com>
2022-10-22 18:51:32 -0300

    feat(shared): create widget ListTile

--- libs/flutter/shared/lib/flutter_shared.dart
+++ libs/flutter/shared/lib/flutter_shared.dart
@@ -4,6 +4,7 @@ export './src/widgets/appbar_widget.dart';
 export './src/widgets/choice_chip_widget.dart';
 export './src/widgets/feedback_page_widget.dart';
 export './src/widgets/generic_label_widget.dart';
+export './src/widgets/list_tile_widget.dart';
 export './src/widgets/skeleton_widget.dart';
 export './src/widgets/skeleton_list_widget.dart';
 export './src/widgets/skeleton_list_tile_widget.dart';

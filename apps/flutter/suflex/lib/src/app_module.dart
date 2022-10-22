2364e49ad286c1fcb43485094d0ab1773653a562
Leandro Mancini <mancinilele@gmail.com>
2022-10-22 02:41:10 -0300

    feat(core): add new route episode

--- apps/flutter/suflex/lib/src/app_module.dart
+++ apps/flutter/suflex/lib/src/app_module.dart
@@ -1,9 +1,11 @@
 import 'package:flutter_characters/flutter_characters.dart';
+import 'package:flutter_episodes/flutter_episodes.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
 class AppModule extends Module {
   @override
   List<ModularRoute> get routes => [
-    ModuleRoute('/', module: CharactersModule())
+    ModuleRoute('/', module: CharactersModule()),
+    ModuleRoute('/episode', module: EpisodesModule())
   ];
 }
